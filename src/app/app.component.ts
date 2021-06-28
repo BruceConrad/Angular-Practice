05a6fbed8674d12fcb9da8b8eca4a59235dbfc9d
Bruce Conrad <sbruceconrad2@gmail.com>
2021-06-28 17:10:18 -0400

    Initial commit with training

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-first',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template: `
+  //   <div>
+  //     {{title}}
+  //     {{sayHello()}}
+  //   </div>
+  // `,
+  // styleUrls: ['./app.component.css']
+  styles: [`
+  div{
+    background-color: #def;
+  }
+  `]
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'my-first-app';
+  active = false;
+  name = 'John Doe'
+
+  submit()
+  {
+    console.log('submitting form');
+  }
+
+  sayHello()
+  {
+    return 'Hi there';
+  }
+
+  changeNameHandler(e:any)
+  {
+    this.name = e.target.value;
+  }
 }
